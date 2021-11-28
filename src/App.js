9e0402a8c5ec5108bed103f8ad0f4b4a96b3a26e
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2021-11-28 13:03:33 -0700

    added pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { VStack } from '@chakra-ui/react';
 import './App.css';
-
+import Home from './pages/Home';
+import About from './pages/About';
 function App() {
-	return <div>Hello</div>;
+	return (
+		<VStack minH='100vh' spacing={0}>
+			<VStack
+				w={['90%', '95%']}
+				minH='100%'
+				spacing={10}
+				m='auto'
+				py={['5', '10']}
+			>
+				<Home />
+				<About />
+			</VStack>
+		</VStack>
+	);
 }
-
 export default App;
