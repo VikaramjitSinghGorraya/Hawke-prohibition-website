8a3a25d647b50f969e44404cf756daaf339814ae
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2021-12-02 17:52:28 -0700

    imported all the styles to index

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 import { theme as chakraTheme } from '@chakra-ui/react';
-import globalTheme from './styles';
+import globalTheme from './globalStyles';
 import colorTheme from './color';
 import buttonTheme from './components/Button';
 import borderTheme from './components/Border';
+import layerTheme from './layerStyles';
+
 const fonts = {
 	...chakraTheme.fonts,
 	heading: 'Playfair Display',
@@ -12,6 +14,9 @@ const fonts = {
 const overrides = {
 	fonts,
 	colors: { ...colorTheme },
+	layerStyles: {
+		...layerTheme,
+	},
 	styles: {
 		...globalTheme,
 		...colorTheme,
