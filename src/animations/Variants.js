5c225af304275b9e02a6c697c9a4bf3856816b2b
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2021-12-04 10:06:39 -0700

    added animation variants,animation to product card

---
+++ src/animations/Variants.js
@@ -0,0 +1,5 @@
+export const popOnView = {
+	visible: { opacity: 1, top: 0, transition: { duration: 0.4, delay: 0.1 } },
+	hidden: { opacity: 0, top: 100 },
+	pop: { scale: 1.1 },
+};
--- src/layout/ProductCard.js
+++ src/layout/ProductCard.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import { VStack, HStack, Image, Text } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { VStack, HStack, Text } from '@chakra-ui/react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { popOnView } from '../animations/Variants';
 
+const MotionVStack = motion(VStack);
 const ProductCard = ({ imageName, productName, productPrice }) => {
+	const controls = useAnimation();
+	const [ref, inView] = useInView();
+
+	useEffect(() => {
+		inView ? controls.start('visible') : controls.start('hidden');
+	}, [controls, inView]);
+
 	return (
-		<VStack
-			minW={['90%', 'auto']}
+		<MotionVStack
+			minW={['90%', '55%', 'auto']}
 			_hover={{ cursor: 'pointer' }}
-			py='4'
-			m='auto'
 			bg='brand.bannerBg'
+			w='20rem'
+			h='20rem'
+			ref={ref}
+			animate={controls}
+			initial='hidden'
+			whileHover='pop'
+			variants={popOnView}
+			position='relative'
 		>
-			<VStack h='100%' w='90%'>
-				<Image
-					bg='transparent'
-					src={imageName}
-					objectFit='contain'
-					h='20rem'
-					w='20rem'
-					m='auto'
-				/>
-			</VStack>
+			<LazyLoadImage
+				style={{
+					objectFit: 'contain',
+					minHeight: '15rem',
+					maxHeight: '15rem',
+				}}
+				effect='blur'
+				src={imageName}
+			/>
+
 			<HStack w='100%' justifyContent='center' layerStyle='footNotes'>
 				<Text as='p'>
 					{productName} <br />${productPrice}/-
 				</Text>
 			</HStack>
-		</VStack>
+		</MotionVStack>
 	);
 };
 
