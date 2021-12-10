7b745bb093e40f4aa0b84bca28ec5aa1d81da3a7
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2021-12-10 16:55:09 -0700

    made the navigate function global

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { VStack } from '@chakra-ui/react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import LearnMore from './pages/LearnMore';
 import Navbar from './layout/Navbar';
 import Footer from './layout/Footer';
 import Home from './pages/Home';
-
+import Cart from './pages/Cart';
 function App() {
+	const navigate = useNavigate();
 	return (
 		<VStack minH='100vh' spacing={5}>
-			<Navbar />
+			<Navbar navigation={navigate} />
 			<VStack w={['90%', '95%']} minH='100%' m='auto' py={['5', '10']}>
 				<Routes>
 					<Route path='/LearnMore' element={<LearnMore />} />
+					<Route path='/Cart' element={<Cart />} />
 					<Route path='/' element={<Home />} />
 				</Routes>
 			</VStack>
